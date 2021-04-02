4848f78db2b211d5e869115cd41ddc898d79f67d
coranos <coranos@users.noreply.github.com>
2021-04-02 18:20:18 -0500

    debugging

--- scripts/web/pages/withdraw-util.js
+++ scripts/web/pages/withdraw-util.js
@@ -58,7 +58,7 @@ const postWithoutCatch = async (context, req, res) => {
 
   if (config.disableWithdraw) {
     const resp = {};
-    resp.status(401);
+    res.status(401);
     resp.message = 'demo mode only, withdrawal is disabled';
     resp.success = false;
     res.send(resp);
