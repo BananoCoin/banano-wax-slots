2c5ec7dea635cc8e356b3cfbb43ca6c29b57f6f1
coranos <coranos@users.noreply.github.com>
2021-04-09 21:19:55 -0500

    v1.0.59 bumping deps

--- scripts/web/pages/play-util.js
+++ scripts/web/pages/play-util.js
@@ -132,7 +132,7 @@ const postWithoutCatch = async (context, req, res) => {
   const badNonce = await nonceUtil.isBadNonce(owner, nonce);
   if (badNonce) {
     const resp = {};
-    resp.errorMessage = `Need to log in again, server side nonce hash has does not match blockchain nonce hash.`;
+    resp.errorMessage = `Nonce mismatch, log in again.`;
     resp.ready = false;
     res.send(resp);
     return;
